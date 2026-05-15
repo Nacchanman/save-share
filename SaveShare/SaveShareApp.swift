d58bbf5ee585f24da98d7eb13b3541955cbfc2cd
Nacchanman <97383661+Nacchanman@users.noreply.github.com>
2026-05-15 20:30:21 +0900

    Remove unused store dependency from app entry

--- SaveShare/SaveShareApp.swift
+++ SaveShare/SaveShareApp.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct SaveShareApp: App {
-    @StateObject private var store = SaveShareStore()
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(store)
         }
     }
 }
